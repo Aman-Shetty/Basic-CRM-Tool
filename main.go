632a262167db90b4f0fdc1966dd49d7a227eb5c9
Aman-Shetty <amanshetty370@gmail.com>
2024-08-26 19:32:21 +0530

    Added function initDatabase

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/Aman-Shetty/Basic-CRM-Tool/database"
 	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
 )
 
 func setupRoutes(app *fiber.App) {
@@ -12,11 +16,20 @@ func setupRoutes(app *fiber.App) {
 }
 
 func initDatabase() {
-
+	var err error
+	database.DBConnection, err = gorm.Open("sqlite3", "leads.db")
+	if err != nil {
+		panic("Failed to Connect to Database.")
+	}
+	fmt.Println("Successfully connected to the Database.")
+	database.DBConnection.AutoMigrate(&lead.Lead{})
+	fmt.Println("Database Migrated")
 }
 
 func main() {
 	app := fiber.New()
+	initDatabase()
 	setupRoutes(app)
 	app.Listen(3000)
+	defer database.DBConnection.Close()
 }
